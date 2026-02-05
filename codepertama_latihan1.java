e38f1b8b5d9c8b2e0f14842636f69893f78bc6cf
Tania Yuniar <d1041231010@student.untan.ac.id>
2026-02-05 14:27:16 +0700

    message

---
+++ codepertama_latihan1.java
@@ -0,0 +1,12 @@
+public class codepertama_latihan1 {
+    public static void main(String[]args) {
+        System.out.println("Nama: Tania Yuniar");
+        System.out.println("NIM :D1041231010");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi: Informatika");
+       
+    } 
+}
+/* Nama: Tania Yuniar */
+/* Nim : D1041231010 */
+/* Tanggal : 5 Februari 2026 */
