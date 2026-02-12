fb69890e5f1309ba90b377839dc8ef87330aaf03
tania-ynr <d1041231010@student.untan.ac.id>
2026-02-12 14:09:04 +0700

    Add files via upload

---
+++ pertemuan_3/p022workingvariabel.java
@@ -0,0 +1,12 @@
+public class p022workingvariabel {
+    public static void main(String[] args) {
+        int angka = 10;
+
+        System.out.println("Nilai awal angka: " + angka);
+
+        angka = 25;
+
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
+
---
+++ pertemuan_3/p023PrimitiveData.java
@@ -0,0 +1,22 @@
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        char nilaiHuruf ='A';
+
+        boolean lulus = true;
+
+        System.out.println("umur            :" + umur);
+        System.out.println("populasi        :" + populasi);
+        System.out.println("Tinggi Badan    :" + tinggi);
+        System.out.println("ipk             :" + ipk);
+        System.out.println("Nilai Huruf     :" + nilaiHuruf);
+        System.out.println("status lulus    :" + lulus);
+
+    }
+}
+
